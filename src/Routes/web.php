06b2bf5ca3579d3f2693355fb19db8078429149b
Grégory COLLIN <gregory@d4rk0s.net>
2022-12-09 21:11:59 +0100

    bugfixes on routes

--- src/Routes/web.php
+++ src/Routes/web.php
@@ -4,8 +4,10 @@ use D4rk0s\Mangopay\Controllers\Card3DS2Callback;
 use D4rk0s\Mangopay\Controllers\CardRegistrationCallback;
 use Illuminate\Support\Facades\Route;
 
-Route::get('mangopay/card-registration-callback', CardRegistrationCallback::class)
-    ->name('mangopay-cardRegistrationCallback');
+Route::group(['middleware'=>['web']],function() {
+    Route::get('mangopay/card-registration-callback', CardRegistrationCallback::class)
+      ->name('mangopay-cardRegistrationCallback');
 
-Route::get('mangopay/3ds2Callback', Card3DS2Callback::class)
-    ->name("mangopay-3ds2Callback");
+    Route::get('mangopay/3ds2Callback', Card3DS2Callback::class)
+      ->name("mangopay-3ds2Callback");
+});
